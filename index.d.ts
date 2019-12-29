2f05c251b12996caa9104ed412b0fbef57ca675a
k4m4 <nikolaskam@gmail.com>
2019-12-29 09:19:46 -0500

    Fix inconsistent indentation

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,21 @@
 declare namespace ethereumRegex {
-	interface Options {
-		/**
-		Only match an exact string. By default, it matches any ETH addresses in a string. Useful with `RegExp#test()` to check if a string is an ETH address.
-		@default false
-		*/
-		readonly exact?: boolean;
+    interface Options {
+        /**
+        Only match an exact string. By default, it matches any ETH addresses in a string. Useful with `RegExp#test()` to check if a string is an ETH address.
+        @default false
+        */
+        readonly exact?: boolean;
     }
 }
 
 /**
-    Returns a regex for matching Ethereum (ETH) addresses.
-    @example
-    ```
-    import ethereumRegex = require('ethereum-regex')
-    ethereumRegex().test('nodejsrocks 0x281055afc982d96fab65b3a49cac8b878184cb16')
-    //=> true
-    ```
+Returns a regex for matching Ethereum (ETH) addresses.
+@example
+```
+import ethereumRegex = require('ethereum-regex')
+ethereumRegex().test('nodejsrocks 0x281055afc982d96fab65b3a49cac8b878184cb16')
+//=> true
+```
 */
 declare function ethereumRegex(options?: ethereumRegex.Options): RegExp;
 
